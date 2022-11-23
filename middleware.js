ebbd0ba231b95099aab354f06a30f499a30bd821
ajiohjesse <ajiohjesse@gmail.com>
2022-11-23 11:38:53 +0100

    Modify middleware

--- middleware.js
+++ middleware.js
@@ -1,44 +1,36 @@
 import { NextResponse } from 'next/server'
 import * as jose from 'jose'
-// import client from './apollo.config'
-import { GET_CURRENT_USER } from './graphql/queries/userQueries'
 
-export function middleware(request) {
-  return NextResponse.redirect(new URL('/about-2', request.nextUrl))
+export async function middleware(request) {
+  const token = request.cookies.get('user')
+  const url = request.url
 
-  // const token = req.cookies.get('user')
-  // const url = req.url
+  let isLoggedIn
 
-  // let isLoggedIn
+  try {
+    await jose
+      .jwtVerify(token.value, new TextEncoder().encode(process.env.JWT))
+      .then(() => {
+        isLoggedIn = true
+      })
+  } catch (error) {
+    isLoggedIn = false
+  }
 
-  // try {
-  //   await jose
-  //     .jwtVerify(token.value, new TextEncoder().encode(process.env.JWT))
-  //     .then(() => {
-  //       isLoggedIn = true
-  //     })
-  // } catch (error) {
-  //   isLoggedIn = false
-  // }
+  /**
+   * Redirect user to login page
+   * if not logged in.
+   */
+  if (!isLoggedIn && url.includes('/dashboard')) {
+    return NextResponse.redirect(new URL('/auth/login', req.nextUrl))
+  }
 
-  // /**
-  //  * Redirect user to login page
-  //  * if not logged in.
-  //  */
-  // if (!isLoggedIn && url.includes('/dashboard')) {
-  //   return NextResponse.redirect(new URL('/auth/login', req.nextUrl))
-  // }
+  /**
+   * Dont login if user is already
+   * logged in.
+   */
 
-  // /**
-  //  * Dont login if user is already
-  //  * logged in.
-  //  */
-
-  // if (isLoggedIn && url.includes('/login')) {
-  //   return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
-  // }
-}
-
-export const config = {
-  matcher: '/about/:path*',
+  if (isLoggedIn && url.includes('/login')) {
+    return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
+  }
 }
